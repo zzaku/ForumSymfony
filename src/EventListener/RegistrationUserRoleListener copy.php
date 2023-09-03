fd63f4fd315a33c784f338502e2f01b8f1b21da6
zzaku <enzo.makked@gmail.com>
2023-09-03 23:36:49 +0200

    auth - security - manage user added

--- src/EventListener/RegistrationUserRoleListener copy.php
+++ src/EventListener/RegistrationUserRoleListener copy.php
@@ -2,7 +2,6 @@
 
 namespace App\EventListener;
 
-use App\Entity\User;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -22,28 +21,5 @@ class RegistrationUserRoleListener
         $this->entityManager = $entityManager;
     }
 
-    public function prePersist(User $user, LifecycleEventArgs $event): void
-    {
-        $token = $this->tokenStorage->getToken();
-        
-        if ($token !== null) {
-            $currentUser = $token->getUser();
-            
-            // Vérifiez si l'utilisateur actuel n'est pas l'administrateur connecté
-            if ($currentUser instanceof User && !$this->authorizationChecker->isGranted('ROLE_ADMIN')) {
-                $email = $user->getEmail();
-   
-                if (strpos($email, '@insider.fr') !== false) {
-                    $user->setRoles(['ROLE_INSIDER']);
-                } elseif (strpos($email, '@collaborator.fr') !== false) {
-                    $user->setRoles(['ROLE_COLLABORATOR']);
-                } elseif (strpos($email, '@external.fr') !== false) {
-                    $user->setRoles(['ROLE_EXTERNAL']);
-                } else {
-                    $user->setRoles(['ROLE_EXTERNAL']);
-                }
-            }
-        }
-    }
 }
 ?>
